3d3dae897ef8b8c681114b58c0e79a1ae5d3ee11
Phuc Anh <75134436+anhhp0@users.noreply.github.com>
2023-12-18 17:05:53 +0700

    upload sr

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import '@/app/ui/global.css';
+//import './ui/global.css';
+import {inter} from '@/app/ui/fonts';
+// import { lusitana } from './ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+      {/* <body className={`${lusitana.className} antialiased`}>{children}</body> */}
+    </html>
+  );
+}
