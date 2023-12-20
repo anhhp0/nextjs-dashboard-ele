ed3ec9ae24d59ba365f2fba6ffd8ae207811ad17
Phuc Anh <75134436+anhhp0@users.noreply.github.com>
2023-12-20 10:32:32 +0700

    add pagination, some hover

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -27,7 +27,7 @@ export function UpdateInvoice({ id }: { id: string }) {
 export function DeleteInvoice({ id }: { id: string }) {
   return (
     <>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
+      <button className="rounded-md border p-2 hover:bg-gray-100 hover:bg-red-600">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
