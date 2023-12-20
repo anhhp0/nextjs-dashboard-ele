23d66a908bbe8408fc3643d38e757e3e4cc0f801
Phuc Anh <75134436+anhhp0@users.noreply.github.com>
2023-12-20 17:19:47 +0700

    handling 404 not found

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,46 +7,46 @@ import { redirect } from 'next/navigation';
 //import { UpdateInvoice } from '../ui/invoices/buttons';
 
 const FormSchema = z.object({
-  id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
-  date: z.string(),
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
-  const amountInCents = amount * 100; //convert amount into cents
-  const date = new Date().toISOString().split('T')[0];
-try{
-  await sql`
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100; //convert amount into cents
+    const date = new Date().toISOString().split('T')[0];
+    try {
+        await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-} catch (error) {
-    return {
-        message: 'Database Error: Failed to Create Invoice.',
-    }
-     };
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
-  //console.group(rawFormData);
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        }
+    };
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+    //console.group(rawFormData);
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
 
-  const amountInCents = amount * 100;
+    const amountInCents = amount * 100;
 
     try {
         await sql`
@@ -58,11 +58,15 @@ export async function updateInvoice(id: string, formData: FormData) {
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
 
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
+
+
+    // Unreachable code block
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
