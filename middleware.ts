5a74f05f782abaaec23666c391d4f1e383e6d535
Phuc Anh <75134436+anhhp0@users.noreply.github.com>
2023-12-21 17:15:28 +0700

    return noAuth

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
-
-export default NextAuth(authConfig).auth;
-
-export const config = {
-  mathcher: ['/((?api|_next/static|_net/image|.*\\.png$).*)'],  
-};
