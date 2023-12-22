7b831dd0ea156a2b7cd3f48bcb7bb7c6694aaa4d
Phuc Anh <75134436+anhhp0@users.noreply.github.com>
2023-12-22 11:10:50 +0700

    add display /Customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { inter } from '@/app/ui/fonts';
+// import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchCustomers } from '@/app/lib/data';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchCustomers();
+  // const allPages = generatePagination(currentPage, totalPages);
+  return (
+    <div className="w-full">
+      <Suspense key={query + currentPage}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
+}
